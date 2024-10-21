2f7ad5dfa1cbb77679f69932344befe420abfda2
ryan terronez <ryanterronez@gmail.com>
2024-10-21 12:39:11 -0500

    updated from ts to js

---
+++ src/components/Box.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+const value = ref('')
+</script>
+
+<template>
+  <button>{{ value }}</button>
+</template>
+
+<style scoped>
+button {
+  font: comic sans ms;
+  font-size: 32px;
+  width: 80px; /* Adjust size as needed */
+  height: 80px; /* Adjust size as needed */
+  border-radius: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: grey;
+}
+</style>
